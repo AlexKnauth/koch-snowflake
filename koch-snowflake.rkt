8ae7300863daf104d9fd2c47972d969feaea8d3f
AlexKnauth <alexander@knauth.org>
2015-02-14 13:50:38 -0500

    change d -> ∆ and use more sweet-exp

--- koch-snowflake.rkt
+++ koch-snowflake.rkt
@@ -1,27 +1,26 @@
 #lang sweet-exp racket/base
 
-(provide (all-from-out
-          "koch-snowflake-simple.rkt"
-          "koch-snowflake-inner-fractal.rkt"
-          ))
+provide
+      all-from-out 
+        "koch-snowflake-simple.rkt"
+        "koch-snowflake-inner-fractal.rkt"
 
-(require 2htdp/image
-         "koch-snowflake-simple.rkt"
-         "koch-snowflake-inner-fractal.rkt"
-         "posn.rkt"
-         "utils.rkt"
-         )
+require 2htdp/image
+        "koch-snowflake-simple.rkt"
+        "koch-snowflake-inner-fractal.rkt"
+        "posn.rkt"
+        "utils.rkt"
 
-(module+ test
-  (snowflake 728)
-  (snowflake/inner-fractal 728)
-  (snowflake/inner-fractal/multi-color 150 '("transparent" "black")) ; inside-out-ish
+module+ test
+  snowflake 728
+  snowflake/inner-fractal 728
+  snowflake/inner-fractal/multi-color 150 '("transparent" "black") ; inside-out-ish
   (snowflake/inner-fractal/multi-color 500 '("red" "green") #:cutoff 12) ; 2-color
   (snowflake/inner-fractal/multi-color 500 '("red" "blue" "green") #:cutoff 12) ; 3-color
   (snowflake/inner-fractal/multi-color 100 '("red" "blue" "green" "orange") #:cutoff 12) ; 4-color
   (snowflake/inner-fractal/multi-color 500 '("red" "orange" "yellow" "green" "blue" "purple")
                                        #:cutoff 10) ; 6-color
-  (add-k-line (empty-scene 1020 520) #:cutoff default-line-cutoff
-              (make-posn 10 510)
-              (make-posn 1010 510))
-  )
+  (add-k-line empty-scene(1020 520) #:cutoff default-line-cutoff
+              posn(10 510)
+              posn(1010 510))
+
--- posn.rkt
+++ posn.rkt
@@ -1,60 +1,50 @@
 #lang sweet-exp racket/base
 
-(provide (all-defined-out))
+provide all-defined-out()
 
-(require racket/match
-         racket/math
-         math/base
-         postfix-dot-notation
-         (prefix-in rkt: racket/base)
-         )
+require racket/match
+        racket/math
+        math/base
+        postfix-dot-notation
+        prefix-in rkt: racket/base
 
 (struct posn (x y) #:transparent #:extra-constructor-name make-posn)
 
-(define p posn)
-(define x posn-x)
-(define y posn-y)
+define p posn
+define x posn-x
+define y posn-y
 
-(define (+ . args)
-  (match args
-    [(list) 0]
-    [(list (? number? args) ...) (sum args)]
-    [(list (posn xs ys) ...) (posn (sum xs) (sum ys))]))
+define (+ . args)
+  match args
+    [list() 0]
+    [list(?[number? args] ...) sum(args)]
+    [list(posn(xs ys) ...) posn(sum(xs) sum(ys))]
 
-(define -
-  (case-lambda
+define -
+  case-lambda
     [(a) {-1 * a}]
-    [(a . rst) {a + (- (apply + rst))}]))
+    [(a . rst) {a + -(apply(+ rst))}]
 
-(define (* . args)
-  (match args
-    [(list) 1]
-    [(list (? number? args) ...) (apply rkt:* args)]
-    [(list-no-order (posn x y) (? real? ns) ...)
-     (define n (apply rkt:* ns))
-     (posn {n * x} {n * y})]))
+define (* . args)
+  match args
+    list() 1
+    list(?[number? args] ...) apply(rkt:* args)
+    list-no-order(posn(x y) ?[real? ns] ...)
+      define n apply(rkt:* ns)
+      posn({n * x} {n * y})
 
 ;; distance : Posn Posn -> Number
 ;; produce the distance between two points
-;; note that here d means delta, not differential
-(define (distance p1 p2)
-  (let ([dx (dx p1 p2)] [dy (dy p1 p2)])
-    √{{dx ^ 2} + {dy ^ 2}}))
+define distance(p1 p2)
+  let ([∆x {p2.x - p1.x}] [∆y {p2.y - p1.y}])
+    √{{∆x ^ 2} + {∆y ^ 2}}
 
-;; dx and dy are helper functions for distance
-;; note that here d means delta, not differential
-;; dx : Posn Posn -> Real
-(define (dx p1 p2)
-  {p2.x - p1.x})
-;; dy : Posn Posn -> Real
-(define (dy p1 p2)
-  {p2.y - p1.y})
-;; d : Posn Posn -> Posn
-(define (d p1 p2)
-  {p2 - p1})
+;; ∆ : Posn Posn -> Posn
+define (∆ p1 p2)
+  {p2 - p1}
 
-(define √ sqrt)
-(define ^ expt)
+define √ sqrt
+define ^ expt
 
 
 
