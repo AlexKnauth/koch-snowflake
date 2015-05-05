1a0a08491b523570472c28e3d8ce30f972d3445f
AlexKnauth <alexander@knauth.org>
2015-05-05 15:43:05 -0400

    use add-line instead of scene+line

--- utils.rkt
+++ utils.rkt
@@ -5,6 +5,7 @@ provide all-defined-out()
 require 2htdp/image
         racket/match
         syntax/parse/define
+        my-cond/iffy
         postfix-dot-notation
         "posn.rkt"
 
@@ -27,6 +28,14 @@ define-simple-macro
 ;; add-simple-line/color : Image Posn Posn Color -> Image
 define
   add-simple-line/color img p1 p2 color
-  scene+line img p1.x p1.y p2.x p2.y color
-
+  my-cond
+    #:let ([x1 p1.x] [y1 p1.y] [x2 p2.x] [y2 p2.y] [w img.image-width] [h img.image-height])
+    if (and {0 <= x1 <= w}
+            {0 <= x2 <= w}
+            {0 <= y1 <= h}
+            {0 <= y2 <= h})
+      add-line img x1 y1 x2 y2 color
+    else
+      eprintf "warning: add-simple-line/color: line outside bounding box\n"
+      img
 
