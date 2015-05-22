4537824931998fe7ced1fb35e03a0d5b1b35fbb7
AlexKnauth <alexander@knauth.org>
2015-05-22 16:54:50 -0400

    add Serpinski star and related stuff

---
+++ serpinski-star-animate-delta-a.rkt
@@ -0,0 +1,52 @@
+#lang postfix-dot-notation sweet-exp racket
+
+require 2htdp/image
+        2htdp/universe
+        my-cond/iffy
+        koch-snowflake/utils
+        koch-snowflake/posn
+        math/base
+
+define cutoff 11
+
+define serpinski-star(r color ∆a)
+  define w {{r * 2} + 10}
+  define h w
+  define MTS rectangle(w h "solid" "transparent")
+  define t(p)
+    posn({p.y + {w / 2}} {(- p.x) + {h / 2}})
+  add-serpinski-star*(MTS r color t ∆a)
+
+define add-serpinski-star*(img r color t ∆a)
+  define img2
+    for/fold ([img img]) ([i (in-range 5)])
+      define a1 {2 * pi * {{i - 1} / 5}}
+      define a2 {2 * pi * {{i + 1} / 5}}
+      define p1 t{r * posn(cos(a1) sin(a1))}
+      define p2 t{r * posn(cos(a2) sin(a2))}
+      add-simple-lines/color img color
+        p1 p2
+  my-cond
+    if {r <= cutoff}
+      img2
+    else
+      define r* {r * {1 - {1 / |phi.0|}}}
+      for/fold ([img img]) ([i (in-range 5)])
+        define a {{2 * pi * {i / 5}} + ∆a}
+        define ∆p
+          {{r - r*} * posn(cos(a) sin(a))}
+        define (t2 p)
+          t{p + ∆p}
+        add-serpinski-star*(img r* color t2 (- ∆a))
+
+module+ test
+  define 2pi/360 {{2 * pi} / 360}
+  define hsh
+    for/hash ([t (in-range 0 360 2)])
+      values
+        t
+        freeze serpinski-star(200 "purple" {t * 2pi/360})
+  animate
+    λ (t)
+      hash-ref hsh
+        modulo {2 * exact-round(t)} 360
---
+++ serpinski-star-switch-sin-cos.rkt
@@ -0,0 +1,42 @@
+#lang postfix-dot-notation sweet-exp racket
+
+require 2htdp/image
+        my-cond/iffy
+        koch-snowflake/utils
+        koch-snowflake/posn
+        math/base
+
+define cutoff 11
+
+define serpinski-star(r color)
+  define w {{r * 2} + 10}
+  define h w
+  define MTS rectangle(w h "solid" "transparent")
+  define t(p)
+    posn({p.y + {w / 2}} {(- p.x) + {h / 2}})
+  add-serpinski-star*(MTS r color t)
+
+define add-serpinski-star*(img r color t)
+  define img2
+    for/fold ([img img]) ([i (in-range 5)])
+      define a1 {2 * pi * {{i - 1} / 5}}
+      define a2 {2 * pi * {{i + 1} / 5}}
+      define p1 t{r * posn(cos(a1) sin(a1))}
+      define p2 t{r * posn(cos(a2) sin(a2))}
+      add-simple-lines/color img color
+        p1 p2
+  my-cond
+    if {r <= cutoff}
+      img2
+    else
+      define r* {r * {1 - {1 / |phi.0|}}}
+      for/fold ([img img2]) ([i (in-range 5)])
+        define a {2 * pi * {i / 5}}
+        define ∆p
+          {{r - r*} * posn(sin(a) cos(a))}
+        define (t2 p)
+          t{p + ∆p}
+        add-serpinski-star*(img r* color t2)
+
+module+ test
+  serpinski-star(200 "purple")
