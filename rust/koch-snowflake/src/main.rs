e3605e6a33a86afed80185a47033fc2beb833e16
AlexKnauth <alexander@knauth.org>
2021-04-14 22:55:52 -0400

    factor out pos

--- rust/koch-snowflake/src/main.rs
+++ rust/koch-snowflake/src/main.rs
@@ -1,32 +1,22 @@
 extern crate image;
 extern crate imageproc;
+mod pos;
 
 use image::{Rgb, RgbImage};
 use imageproc::drawing::draw_antialiased_line_segment_mut;
 use imageproc::pixelops::interpolate;
+use pos::*;
 
-type Pos = (f32, f32);
 type Color = Rgb<u8>;
 
 const DEFAULT_LINE_CUTOFF: f32 = 4f32;
-const TAU_OVER_6: f32 = std::f32::consts::FRAC_PI_3;
 const WHITE: Color = Rgb([255u8, 255u8, 255u8]);
 const BLACK: Color = Rgb([0u8, 0u8, 0u8]);
 
-fn sqr(a: f32) -> f32 { a * a }
-fn pos_plus(a: Pos, b: Pos) -> Pos { (a.0 + b.0, a.1 + b.1) }
-fn pos_delta(a: Pos, b: Pos) -> Pos { (b.0 - a.0, b.1 - a.1) }
-fn pos_div(a: Pos, s: f32) -> Pos { (a.0 / s, a.1 / s) }
-fn pos_dist1(a: Pos) -> f32 { f32::sqrt(sqr(a.0) + sqr(a.1)) }
-fn pos_rotate(a: Pos, r: f32) -> Pos {
-    ((f32::cos(r) * a.0) + (f32::sin(r) * a.1),
-     (- f32::sin(r) * a.0) + (f32::cos(r) * a.1))
-}
-
 fn draw_line_segment_mut(img: &mut RgbImage, a: Pos, b: Pos, c: Color) {
     draw_antialiased_line_segment_mut(img,
-        (a.0.round() as i32, a.1.round() as i32),
-        (b.0.round() as i32, b.1.round() as i32),
+        pos_round_i32(a),
+        pos_round_i32(b),
         c,
         interpolate);
 }
@@ -57,7 +47,7 @@ fn koch_curve(img: &mut RgbImage, a: Pos, b: Pos, c: Color) {
     } else {
         let ab3 = pos_div(ab, 3f32);
         let l = pos_plus(a, ab3);
-        let t = pos_plus(l, pos_rotate(ab3, TAU_OVER_6));
+        let t = pos_plus(l, pos_rotate(ab3, FRAC_TAU_6));
         let r = pos_plus(l, ab3);
         koch_curve(img, a, l, c);
         koch_curve(img, l, t, c);
@@ -73,7 +63,7 @@ fn koch_curve_cave(img: &mut RgbImage, a: Pos, b: Pos, c: Color) {
     } else {
         let ab3 = pos_div(ab, 3f32);
         let l = pos_plus(a, ab3);
-        let t = pos_plus(l, pos_rotate(ab3, TAU_OVER_6));
+        let t = pos_plus(l, pos_rotate(ab3, FRAC_TAU_6));
         let r = pos_plus(l, ab3);
         koch_curve_cave(img, t, a, c);
         koch_curve_cave(img, b, t, c);
@@ -88,8 +78,7 @@ fn koch_curve_cave2(img: &mut RgbImage, a: Pos, b: Pos, c: Color) {
     } else {
         let ab3 = pos_div(ab, 3f32);
         let l = pos_plus(a, ab3);
-        let t = pos_plus(l, pos_rotate(ab3, TAU_OVER_6));
-        let v = pos_plus(l, pos_rotate(ab3, -TAU_OVER_6));
+        let t = pos_plus(l, pos_rotate(ab3, FRAC_TAU_6));
         let r = pos_plus(l, ab3);
         koch_curve_cave2(img, t, a, c);
         koch_curve_cave2(img, b, t, c);
@@ -105,8 +94,8 @@ fn koch_curve_reflected(img: &mut RgbImage, a: Pos, b: Pos, c: Color) {
     } else {
         let ab3 = pos_div(ab, 3f32);
         let l = pos_plus(a, ab3);
-        let t = pos_plus(l, pos_rotate(ab3, TAU_OVER_6));
-        let v = pos_plus(l, pos_rotate(ab3, -TAU_OVER_6));
+        let t = pos_plus(l, pos_rotate(ab3, FRAC_TAU_6));
+        let v = pos_plus(l, pos_rotate(ab3, -FRAC_TAU_6));
         let r = pos_plus(l, ab3);
         draw_line_segment_mut(img, a, b, c);
         koch_curve_reflected(img, a, l, c);
@@ -125,8 +114,8 @@ fn koch_curve_reflected2(img: &mut RgbImage, a: Pos, b: Pos, c: Color) {
     } else {
         let ab3 = pos_div(ab, 3f32);
         let l = pos_plus(a, ab3);
-        let t = pos_plus(l, pos_rotate(ab3, TAU_OVER_6));
-        let v = pos_plus(l, pos_rotate(ab3, -TAU_OVER_6));
+        let t = pos_plus(l, pos_rotate(ab3, FRAC_TAU_6));
+        let v = pos_plus(l, pos_rotate(ab3, -FRAC_TAU_6));
         let r = pos_plus(l, ab3);
         koch_curve_reflected2(img, a, l, c);
         koch_curve_reflected2(img, l, t, c);
---
+++ rust/koch-snowflake/src/pos.rs
@@ -0,0 +1,23 @@
+
+pub type Pos = (f32, f32);
+
+pub const FRAC_TAU_6: f32 = std::f32::consts::FRAC_PI_3;
+
+pub fn sqr(a: f32) -> f32 { a * a }
+
+pub fn pos_plus(a: Pos, b: Pos) -> Pos { (a.0 + b.0, a.1 + b.1) }
+
+pub fn pos_delta(a: Pos, b: Pos) -> Pos { (b.0 - a.0, b.1 - a.1) }
+
+pub fn pos_div(a: Pos, s: f32) -> Pos { (a.0 / s, a.1 / s) }
+
+pub fn pos_dist1(a: Pos) -> f32 { f32::sqrt(sqr(a.0) + sqr(a.1)) }
+
+pub fn pos_rotate(a: Pos, r: f32) -> Pos {
+    ((f32::cos(r) * a.0) + (f32::sin(r) * a.1),
+     (- f32::sin(r) * a.0) + (f32::cos(r) * a.1))
+}
+
+pub fn pos_round_i32(a: Pos) -> (i32, i32) {
+    (a.0.round() as i32, a.1.round() as i32)
+}
